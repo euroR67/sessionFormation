99eb03c300fa3b830f76bc85f91f183e66ff6893
euroR67 <mchamaev67@gmail.com>
2023-09-20 23:11:23 +0200

    fonction supprimer session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Form\SessionType;
 use App\Repository\ModulesRepository;
 use App\Repository\SessionRepository;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +22,50 @@ class SessionController extends AbstractController
         return $this->render('session/index.html.twig', [
             'sessions' => $sessions,
         ]);
+    }
+
+    // Méthode pour ajouter une session et pour modifier une session
+    #[Route('/session/add', name: 'new_session')]
+    #[Route('/session/{id}/edit', name: 'edit_session')]
+    public function new_edit(Session $session = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$session) {
+            $session = new Session();
+        }
+
+        $form = $this->createForm(SessionType::class, $session);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $session = $form->getData();
+
+            // On ajoute la session à chaque stagiaire
+            foreach ($session->getStagiaires() as $stagiaire) {
+                $stagiaire->addSession($session);
+                $entityManager->persist($stagiaire);
+            }
+
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session');
+        }
+
+        return $this->render('session/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    // Méthode pour supprimer une session
+    #[Route('/session/{id}/delete', name: 'delete_session')]
+    public function delete(Session $session, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($session);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_session');
     }
 
     // Méthode pour afficher le détail d'une session et les modules associés
---
+++ src/Form/SessionType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Formateur;
+use App\Entity\Formation;
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_session', TextType::class)
+            ->add('nbPlace', NumberType::class)
+            ->add('dateSession', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('dateFin', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('stagiaires', EntityType::class, [
+                'class' => Stagiaire::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'nom_formation',
+            ])
+            ->add('formateur', EntityType::class, [
+                'class' => Formateur::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
